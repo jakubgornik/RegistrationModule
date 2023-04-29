62fb901f55df52c4bf6f5edf501150d48799f4cd
revinto <jakub.gornikk@gmail.com>
2023-04-29 16:34:03 +0200

    Inputs state

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,41 @@ import {
   TextField,
   Box,
   Autocomplete,
-  useTheme,
-  useMediaQuery,
   Container,
+  FormControl,
 } from "@mui/material";
+
+import { useState } from "react";
 
 const sxContainer = {
   backgroundColor: "#F7F7F7",
-  height: "85vh",
   maxWidth: { xs: "70vw", sm: "75vw", xl: "80vw" },
   mx: "auto",
   padding: "0 !important",
-  border: "0.5px #222222 solid",
+  border: "0.5px #333333 solid",
   borderRadius: "11px 11px 0px 0px",
 };
 
 const sxHeader = {
   display: "flex",
-  "justify-content": "center",
-  "align-items": "center",
-  height: ["4.5rem", "4rem"],
+  justifyContent: "center",
+  alignItems: "center",
+  height: ["5rem", "4rem"],
   backgroundColor: "#333333",
   borderRadius: "9px 9px 0px 0px",
+};
+
+const sxSubmitButton = {
+  width: "20%",
+  alignSelf: "flex-end",
+  backgroundColor: "#333333",
+  borderRadius: "9px",
+  mt: 2,
+  ":hover": {
+    opacity: "80%",
+    backgroundColor: "#333333",
+    transition: "1s",
+  },
 };
 
 const sxFormTitle = {
@@ -36,15 +49,17 @@ const sxFormTitle = {
 
 const sxInnerBox = {
   display: "flex",
-  "flex-direction": "column",
-  "flex-wrap": "wrap",
+  flexDirection: "column",
+  flexWrap: "wrap",
   maxWidth: "80%",
   mx: "auto",
-  pt: 4,
+  py: 5,
+  px: 4,
 };
 
 const sxInputs = {
   mb: 2,
+  backdropFilter: "blur(16px)",
   "& .MuiFilledInput-root:after": {
     borderBottom: "2px solid #777777",
   },
@@ -53,62 +68,136 @@ const sxInputs = {
   },
 };
 
+const position: string[] = [
+  "Administrator",
+  "Dyrektor",
+  "Inspektor",
+  "Kierownik",
+  "Księgowy",
+  "Pełnomocnik",
+];
+
+interface FormValues {
+  password: string;
+  repeatedPassword: string;
+  nip: number;
+  email: string;
+  position: string;
+  phone: string;
+}
+
 export default function Home() {
+  const [formData, setFormData] = useState<FormValues>({
+    password: "",
+    repeatedPassword: "",
+    nip: 0,
+    email: "",
+    position: "",
+    phone: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleAutocomplete = (
+    event: React.ChangeEvent<{}>,
+    value: string | null
+  ) => {
+    if (value !== null) {
+      setFormData((prevValues) => ({
+        ...prevValues,
+        ["position"]: value,
+      }));
+    }
+  };
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted with values:", formData);
+  };
+
   return (
     <Container sx={sxContainer}>
       <Box sx={sxHeader}>
         <Typography variant="h5" component="h2" sx={sxFormTitle}>
-          Please enter your data
+          Moduł rejestracji użytkownika
         </Typography>
       </Box>
       <Box sx={sxInnerBox}>
-        <TextField
-          variant="filled"
-          name="password-input"
-          label="Hasło"
-          sx={sxInputs}
-          required
-        />
-        <TextField
-          variant="filled"
-          name="repeatPassword-input"
-          label="Powtórz Hasło"
-          sx={sxInputs}
-          required
-        />
-        <TextField
-          variant="filled"
-          name="nip-input"
-          label="NIP"
-          sx={sxInputs}
-          required
-        />
-        <TextField
-          variant="filled"
-          name="email-input"
-          label="Email"
-          type="email"
-          sx={sxInputs}
-          required
-        />
-        <Autocomplete
-          sx={sxInputs}
-          options={["Option 1", "Option 2", "Option 3"]}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="filled"
-              required
-              label="Stanowisko"
-            />
-          )}
-        />
-        <TextField
-          variant="filled"
-          name="phone-input"
-          label="Telefon"
-          sx={sxInputs}
-        />
+        <FormControl component="form" onSubmit={handleFormSubmit}>
+          <TextField
+            sx={sxInputs}
+            onChange={handleInputChange}
+            variant="filled"
+            name="password"
+            type="password"
+            label="Hasło"
+            required
+          />
+          <TextField
+            sx={sxInputs}
+            onChange={handleInputChange}
+            variant="filled"
+            name="repeatedPassword"
+            label="Powtórz Hasło"
+            type="password"
+            required
+          />
+          <TextField
+            sx={sxInputs}
+            onChange={handleInputChange}
+            variant="filled"
+            name="nip"
+            label="NIP"
+            type="number"
+            required
+          />
+          <TextField
+            sx={sxInputs}
+            onChange={handleInputChange}
+            variant="filled"
+            name="email"
+            label="Email"
+            type="email"
+            autoComplete="off"
+            required
+          />
+          <Autocomplete
+            sx={sxInputs}
+            options={position}
+            onChange={handleAutocomplete}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="filled"
+                name="position"
+                label="Stanowisko"
+                required
+              />
+            )}
+          />
+          <TextField
+            sx={sxInputs}
+            onChange={handleInputChange}
+            variant="filled"
+            name="phone"
+            label="Telefon"
+            type="tel"
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            sx={sxSubmitButton}
+            style={{ textTransform: "none" }}
+          >
+            Dalej
+          </Button>
+        </FormControl>
       </Box>
     </Container>
   );
